a3918c6ae94cb0fe814003d3712b60c1ab5a1f41
katplee <katpelee@gmail.com>
2021-12-19 22:57:21 +0900

    Resume section layout implementation

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll
 }, appearOptions);
 
 appearOnScroll.observe(slideshow);
+
+//Media selection
+const mainMedia = document.querySelector(".main-media-container");
+const otherMedia = document.querySelectorAll('.other-media-wrapper');
+
+otherMedia.forEach(medium =>{
+    medium.addEventListener("click", ()=>{
+        mainMedia.childNodes[1].src = medium.childNodes[1].src;
+    });
+})
